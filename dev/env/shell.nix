3d0247fe4285646c20ca165fdb6105bd59b1d2d8
Timothy DeHerrera <tim@nrdxp.dev>
2025-10-06 20:01:43 -0600

    fix(shell): fetch and set PROTO_ROOT for snix
    Fetches protos and sets up necessary vars for snix crates to "just build"
    inside of the devshell.

--- dev/env/shell.nix
+++ dev/env/shell.nix
@@ -1,9 +1,18 @@
 let
   inherit (atom) pkgs;
   toolchain = atom.fenix.fromToolchainFile { file = "${mod}/rust-toolchain.toml"; };
+
+  protos = pkgs.fetchFromGitHub {
+    owner = "nrdxp";
+    repo = "snix";
+    rev = "protos";
+    hash = "sha256-1ZsRIY/n8r9eJvRRO71+cENsdwTolVH7ACWUr0cfncI=";
+  };
 in
 pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
   RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+  PROTO_ROOT = protos;
+  SNIX_BUILD_SANDBOX_SHELL = "/bin/sh";
   packages =
     with pkgs;
     [
